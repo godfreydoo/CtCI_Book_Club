caabf3bbc9b51f4832bf6c5db8371bccb346cdec
godfreydoo <godfrey.doo@gmail.com>
2021-07-30 20:58:28 +0800

    checkPermutations

--- Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
+++ Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
@@ -1,7 +1,18 @@
 //Given two strings, write a method to decide if one is a permutation of the other.
 
+/*
+1. case sensitive?
+2. care about whitespace?
+*/
+
 const checkPermutations = (str1, str2) => {
-  //Please code here
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  var sortedStr1 = str1.split('').sort().join('');
+  var sortedStr2 = str2.split('').sort().join('');
+
+  return sortedStr1 === sortedStr2;
 };
 
 module.exports = checkPermutations;
