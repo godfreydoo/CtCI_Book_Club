41dc4fe95256bc116899d6ef4f13df785d1ee404
godfreydoo <godfrey.doo@gmail.com>
2021-07-31 07:54:09 +0800

    urlify

--- Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
+++ Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
@@ -5,14 +5,39 @@
 2. care about whitespace?
 */
 
+// const checkPermutations = (str1, str2) => {
+//   if (str1.length !== str2.length) {
+//     return false;
+//   }
+//   var sortedStr1 = str1.split('').sort().join('');
+//   var sortedStr2 = str2.split('').sort().join('');
+
+//   return sortedStr1 === sortedStr2;
+// };
+
 const checkPermutations = (str1, str2) => {
   if (str1.length !== str2.length) {
     return false;
   }
-  var sortedStr1 = str1.split('').sort().join('');
-  var sortedStr2 = str2.split('').sort().join('');
+  var obj = {};
+  for (var i = 0; i < str1.length; i++) {
+    let value = str1[i];
+    if (obj[value]) {
+      obj[value] += 1;
+    } else {
+      obj[value] = 1;
+    }
+  }
 
-  return sortedStr1 === sortedStr2;
-};
+  for (var j = 0; j < str2.length; j++) {
+    let value = str2[j];
+    obj[value] -= 1;
+    if (obj[value] < 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 
 module.exports = checkPermutations;
--- Ch 1 - Arrays and Strings/3-URLify/URLify.js
+++ Ch 1 - Arrays and Strings/3-URLify/URLify.js
@@ -1,7 +1,25 @@
 //Write a method to replace all spaces in a string with '%20'.
 
+/*
+time: O(n)
+space: O(n)
+*/
+
 const URLify = (str) => {
-  //You write code here
+  // return str.trim().split(' ').join('%20');
+  var urlStr = '';
+  var trimmedStr = str.trim();
+  for (var i = 0; i < trimmedStr.length; i++) {
+    let current = trimmedStr[i];
+    let right = trimmedStr[i + 1];
+
+    if (current !== ' ') {
+      urlStr += current;
+    } else {
+      urlStr += '%20';
+    }
+  }
+  return urlStr;
 };
 
 module.exports = URLify;
