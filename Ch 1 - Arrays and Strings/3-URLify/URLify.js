0e06640a95d063483c11ddddbd4ef9bbd833f37f
godfreydoo <godfrey.doo@gmail.com>
2021-07-31 07:58:01 +0800

    urlify

--- Ch 1 - Arrays and Strings/3-URLify/URLify.js
+++ Ch 1 - Arrays and Strings/3-URLify/URLify.js
@@ -7,12 +7,11 @@ space: O(n)
 
 const URLify = (str) => {
   // return str.trim().split(' ').join('%20');
+  // ^ method returns new string & new array & new string at the end
   var urlStr = '';
   var trimmedStr = str.trim();
   for (var i = 0; i < trimmedStr.length; i++) {
     let current = trimmedStr[i];
-    let right = trimmedStr[i + 1];
-
     if (current !== ' ') {
       urlStr += current;
     } else {
