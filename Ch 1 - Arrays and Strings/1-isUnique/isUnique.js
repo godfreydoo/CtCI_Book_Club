c2e431594fb2aa10e489a600645bfa8fb840e436
godfreydoo <godfrey.doo@gmail.com>
2021-07-30 20:56:44 +0800

    isUnique

--- Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
+++ Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
@@ -1,7 +1,20 @@
 //Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
+/*
+1. is it case sensitive?
+2. ASCII or uncide?
+*/
+
 function isUnique(str) {
-  //code
+  var obj = {};
+  for (var i = 0; i < str.length; i++) {
+    let value = str[i];
+    if (obj[value]) {
+      return false;
+    }
+    obj[value] = true;
+  }
+  return true;
 }
 
 module.exports = isUnique;
