3a240310258d444f2f075c17f5f08281e6cbdb4a
godfreydoo <godfrey.doo@gmail.com>
2021-08-07 10:23:17 +0800

    linked lists

--- Ch 2 - Linked Lists/1-removeDups/removeDups.js
+++ Ch 2 - Linked Lists/1-removeDups/removeDups.js
@@ -1,6 +1,8 @@
 // Write code to remove duplicates from an unsorted linked list.
 
 // Follow up: To reduce space complexity How would you solve this problem if a temporary buffer is not allowed? What does this do to time complexity?
+// To reduce space complexity, we can use two pointers, where one will traverse the list to search for duplicates
+// although this method will reduce the space complexity to O(1), it will increase the time complexity to (O n^2)
 
 /*
 Definition of a node in a linked list
@@ -11,7 +13,20 @@ function Node(val) {
 */
 
 function removeDups(head) {
-  // solution
+  var obj = {};
+  let prev = null;
+  let node = head;
+  while (node) {
+    if (obj[node.val]) {
+      prev.next = node.next;
+    } else {
+      obj[node.val] = true;
+      prev = node;
+    }
+    node = node.next;
+  }
+  return head;
 }
+// takes O(n) time wher n is the number of elements in the linked list
 
 module.exports = removeDups;
