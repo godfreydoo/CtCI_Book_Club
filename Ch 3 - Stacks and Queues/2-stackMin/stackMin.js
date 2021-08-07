12f660c5837c0e7386248beb16ead06915c41bd5
godfreydoo <godfrey.doo@gmail.com>
2021-08-07 13:29:10 +0800

    stack min

--- Ch 3 - Stacks and Queues/2-stackMin/stackMin.js
+++ Ch 3 - Stacks and Queues/2-stackMin/stackMin.js
@@ -5,19 +5,30 @@ var Stack = require('./../util/Stack');
 class StackMin extends Stack {
   constructor() {
     super();
-    // finish this
+      this._minStack = new Stack();
   }
 
   push(value) {
-    //
+    if (value <= this.min()) {
+      this._minStack.push(value);
+    }
+    super.push(value);
   }
 
   pop() {
-    //
+    var value = super.pop();
+    if (value == this.min()) {
+      this._minStack.pop();
+    }
+    return value;
   }
 
   min() {
-    //
+    if (this._minStack.isEmpty()) {
+      return null;
+    } else {
+      return this._minStack.peek();
+    }
   }
 }
 
